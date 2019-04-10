0c1564709ef0ff9893fb9415bd2aba6d99cc0f40
Jérôme BALIEUX <jerome.balieux@axiocode.com>
2019-04-10 17:07:20 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyDQzDJk6xHed4IztXsEJu-HAiZYps1fqgM",
+      authDomain: "bookshelves-f4f8a.firebaseapp.com",
+      databaseURL: "https://bookshelves-f4f8a.firebaseio.com",
+      projectId: "bookshelves-f4f8a",
+      storageBucket: "bookshelves-f4f8a.appspot.com",
+      messagingSenderId: "890173184593"
+    };
+    firebase.initializeApp(config);
+  }
 }
